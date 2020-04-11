cac0582859977e5efca5bc38745b21e67e9c7aed
canangok <canangok186@gmail.com>
2020-04-11 21:43:28 +0300

    MVCWebUI created, template added

---
+++ TestProject.MVCWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestProject.MVCWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
